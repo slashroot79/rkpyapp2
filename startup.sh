f0879695f2312de7fa2b56bf07925752a61754a4
Ragu Karuturi <ragukaruturi@microsoft.com>
2025-06-08 17:56:03 -0500

    changes

--- startup.sh
+++ startup.sh
@@ -2,23 +2,35 @@
 set -e
 
 echo ">>> Installing GLIBC 2.34 (if needed)"
-GLIBC_VERSION="2.34"
-GLIBC_PREFIX="$HOME/glibc"
-mkdir -p $GLIBC_PREFIX
-cd /tmp
+sudo apt update
+sudo apt install build-essential gawk bison
+wget http://ftp.gnu.org/gnu/libc/glibc-2.34.tar.gz
+tar -xzf glibc-2.34.tar.gz
+mkdir glibc-build
+cd glibc-build
 
-curl -sSL http://ftp.gnu.org/gnu/libc/glibc-$GLIBC_VERSION.tar.gz | tar zx
-cd glibc-$GLIBC_VERSION
-
-mkdir build && cd build
-../configure --prefix=$GLIBC_PREFIX
+../glibc-2.34/configure --prefix=$HOME/glibc-2.34
 make -j$(nproc)
 make install
+
+cp -r ~/glibc-2.34/lib /home/site/wwwroot
+
+
+export LD_LIBRARY_PATH=/home/site/wwwroot:$LD_LIBRARY_PATH
 
 echo "Activating virtualenv"
 source venv/bin/activate
 
-echo ">>> setting LD_LIBRARY_PATH and Starting Gunicorn server"
-export LD_LIBRARY_PATH="$GLIBC_PREFIX/lib:$LD_LIBRARY_PATH"
+# Start with Daphne instead of Gunicorn
+exec daphne -b 0.0.0.0 -p 8000 myproject.asgi:application
+
 echo $LD_LIBRARY_PATH
-gunicorn myapp.wsgi --bind=0.0.0.0:${PORT:-8000}
+
+
+# echo "Activating virtualenv"
+# source venv/bin/activate
+
+# echo ">>> setting LD_LIBRARY_PATH and Starting Gunicorn server"
+# export LD_LIBRARY_PATH="$GLIBC_PREFIX/lib:$LD_LIBRARY_PATH"
+# echo $LD_LIBRARY_PATH
+# gunicorn myapp.wsgi --bind=0.0.0.0:${PORT:-8000}
