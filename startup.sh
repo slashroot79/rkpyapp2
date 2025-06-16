1fd15378f2defc77b165a3ea75da8cbd89f37a4c
Ragu Karuturi <ragukaruturi@microsoft.com>
2025-06-16 13:31:20 -0500

    custom glibc added

--- startup.sh
+++ startup.sh
@@ -1,43 +1,16 @@
 #!/bin/bash
-set -e
 
-export DEBIAN_FRONTEND=noninteractive
+# Make sure your custom glibc is used only by your app
+export LD_LIBRARY_PATH=/home/site/wwwroot/lib:$LD_LIBRARY_PATH
 
-echo ">>> Installing GLIBC 2.34 (if needed)"
-apt-get update -y
-apt-get install -y make build-essential gawk bison
-wget http://ftp.gnu.org/gnu/libc/glibc-2.34.tar.gz
-tar -xzf glibc-2.34.tar.gz
-mkdir glibc-build
-cd glibc-build
-echo "********************************************** downloaded glibc 2.34"
+# export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/venv/lib/site-packages"
+source /home/site/wwwroot/venv/bin/activate
 
-../glibc-2.34/configure --prefix=$HOME/glibc-2.34
-make -j$(nproc)
-make install
+# Log glibc version for verification
+/home/site/wwwroot/lib/ld-linux-x86-64.so.2 \
+    --library-path /home/site/wwwroot/lib \
+    /home/site/wwwroot/lib/libc.so.6 > /home/site/wwwroot/daphne-log.txt 2>&1
 
-echo "********************************************** isntalled glibc 2.34"
-cp -r ~/glibc-2.34/lib /home/site/wwwroot
-
-
-export LD_LIBRARY_PATH=/home/site/wwwroot:$LD_LIBRARY_PATH
-
-echo "********************************************** ld lib path"
-echo ${LD_LIBRARY_PATH}
-
-echo "Activating virtualenv"
-source venv/bin/activate
-
-# Start with Daphne instead of Gunicorn
-exec daphne -b 0.0.0.0 -p 8000 myproject.asgi:application
-
-echo $LD_LIBRARY_PATH
-
-
-# echo "Activating virtualenv"
-# source venv/bin/activate
-
-# echo ">>> setting LD_LIBRARY_PATH and Starting Gunicorn server"
-# export LD_LIBRARY_PATH="$GLIBC_PREFIX/lib:$LD_LIBRARY_PATH"
-# echo $LD_LIBRARY_PATH
-# gunicorn myapp.wsgi --bind=0.0.0.0:${PORT:-8000}
+# Start the ASGI server using daphne
+# daphne -b 0.0.0.0 -p 8000 myproject.asgi:application >> /home/site/wwwroot/daphne-log.txt 2>&1
+gunicorn myapp.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
